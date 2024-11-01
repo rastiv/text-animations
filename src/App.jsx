b4525cb0ad04a393d2167de03c3d261c0f9c9c50
Raycho Ivanov <rastiv.bg@gmail.com>
2024-11-01 15:01:08 +0200

    make wrapper flex-col

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import TypeWriter from "./components/TypeWriter";
 
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-neutral-100 px-8 py-24">
+    <div className="flex flex-col h-screen items-center justify-center bg-neutral-100 px-8 py-24">
       <div className="w-full max-w-xl space-y-6">
         <TypeWriter
           label="Questions"
