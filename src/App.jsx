995e62f3853709d619cd973f3569effd99297791
Raycho Ivanov <rastiv.bg@gmail.com>
2024-11-11 11:13:31 +0200

    remove link box

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import TypeWriterExamples from "./examples/TypeWriterExamples";
 import FlipTextExample from "./examples/FlipTextExample";
-import LinkBoxExample from "./examples/LinkBoxExample";
 
 function App() {
   return (
     <div className="flex flex-col h-screen items-center justify-center bg-neutral-100 px-8 py-24">
       <div className="w-full max-w-xl space-y-6">
         <FlipTextExample />
-        <hr className="border-neutral-650" />
-        <LinkBoxExample />
         <hr className="border-neutral-650" />
         <TypeWriterExamples />
       </div>
